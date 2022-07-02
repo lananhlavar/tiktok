ed0e98fed1783aa8be1284a52d1ef1f27885b192
Lan Anh <lananhlavar@gmail.com>
2022-07-02 18:38:21 +0700

    doitenfile

---
+++ src/components/Button/Button.js
@@ -0,0 +1,79 @@
+import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
+import styles from './Button.module.scss';
+const cx = classNames.bind(styles);
+function Button({
+    to,
+    href,
+    primary = false,
+    text = false,
+    disable = false,
+    rounded = false,
+    small = false,
+    large = false,
+    outline = false,
+    children,
+    leftIcon,
+    rightIcon,
+    onClick,
+    className,
+    ...passProps
+}) {
+    let Comp = 'button';
+    const props = {
+        onClick,
+        ...passProps,
+    };
+    // Remove event listener when btn disable
+    if (disable) {
+        Object.keys(props).forEach((key) => {
+            if (key.startsWith('on') && typeof props[key] === 'function') {
+                delete props[key];
+            }
+        });
+    }
+    if (to) {
+        props.to = to;
+        Comp = Link;
+    } else if (href) {
+        props.href = href;
+        Comp = 'a';
+    }
+
+    const classes = cx('wrapper', {
+        primary,
+        outline,
+        small,
+        large,
+        text,
+        disable,
+        rounded,
+        [className]: className,
+    });
+    return (
+        <Comp className={classes} {...props}>
+            {leftIcon && <span className={cx('icon')}>{leftIcon}</span>}
+            <span className={cx('title')}>{children}</span>
+            {rightIcon && <span className={cx('icon')}>{rightIcon}</span>}
+        </Comp>
+    );
+}
+Button.propTypes = {
+    to: PropTypes.string,
+    href: PropTypes.string,
+    primary: PropTypes.bool,
+    outline: PropTypes.bool,
+    text: PropTypes.bool,
+    disable: PropTypes.bool,
+    rounded: PropTypes.bool,
+    small: PropTypes.bool,
+    large: PropTypes.bool,
+    children: PropTypes.node.isRequired,
+    className: PropTypes.string,
+    leftIcon: PropTypes.node,
+    rightIcon: PropTypes.node,
+    onClick: PropTypes.func,
+};
+
+export default Button;
