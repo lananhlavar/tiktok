b65bbc8204ffb225f7212625f960ab787f8ba415
Lan Anh <lananhlavar@gmail.com>
2022-07-02 09:42:15 +0700

    toi uu 1

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
-import routesConfig from '~/config/routes';
+import config from '~/config';
 //Layout
-import { HeaderOnly } from '~/components/Layout';
+import { HeaderOnly } from '~/layouts';
 
 //Pages
 import Home from '~/pages/Home';
@@ -12,24 +12,24 @@ import Search from '~/pages/Search';
 //Public routes
 const publicRoutes = [
     {
-        path: routesConfig.home,
+        path: config.routes.home,
         component: Home,
     },
     {
-        path: routesConfig.following,
+        path: config.routes.following,
         component: Following,
     },
     {
-        path: routesConfig.profile,
+        path: config.routes.profile,
         component: Profile,
     },
     {
-        path: routesConfig.upload,
+        path: config.routes.upload,
         component: Upload,
         layout: HeaderOnly,
     },
     {
-        path: routesConfig.search,
+        path: config.routes.search,
         component: Search,
         layout: null,
     },
